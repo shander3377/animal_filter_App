4b6d8b3ea096b0272285d25871e3d9499f4ed08b
shander3377 <65767417+shander3377@users.noreply.github.com>
2022-10-02 01:35:24 +0530

    now has ony one component

--- Components/Filter.js
+++ Components/Filter.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Image, View, Text } from "react-native";
-var Filter9= (props) => {
+var Filter = (props) => {
 	var faceHeight = props["face"]["bounds"]["size"]["height"];
 	var faceWidth = props["face"]["bounds"]["size"]["width"];
 	var leftEyePosition = props["face"]["LEFT_EYE"];
-    var rightEyePosition = props["face"]["RIGHT_EYE"];
-    console.log( props["face"]["bounds"])
+	var rightEyePosition = props["face"]["RIGHT_EYE"];
+	console.log(props["face"]["bounds"]);
 	var noseBasePosition = props["face"]["NOSE_BASE"];
-    
-    const filterWidth = faceWidth * 3.5;
-    const filterHeight = faceHeight * 0.7;
+	var left = props["left"] + 0.1;
+	var right = props["right"];
+	var top = props["top"];
+	var img = props["img"];
+	var width = props["width"];
+	var height = props["height"];
+
+	const filterWidth = faceWidth * width;
+	const filterHeight = faceHeight * height;
 	const transformAngle = (
 		angleRad = Math.atan(
 			(rightEyePosition.y - leftEyePosition.y) /
@@ -17,16 +23,16 @@ var Filter9= (props) => {
 		)
 	) => (angleRad * 100) / Math.PI;
 	return (
-        <View
-        style={{
-          position: "absolute",
-          left: leftEyePosition.x - filterWidth * 0.576,
-          right: rightEyePosition.x - filterWidth * 0.1,
-          top: noseBasePosition.y - filterHeight * 1.25
-        }}
-      >
+		<View
+			style={{
+				position: "absolute",
+				left: leftEyePosition.x - filterWidth * left,
+				right: rightEyePosition.x - filterWidth * right,
+				top: noseBasePosition.y - filterHeight * top,
+			}}
+		>
 			<Image
-				source={require("../assets/hat-pic2.png")}
+				source={img}
 				style={{
 					width: filterWidth,
 					height: filterHeight,
@@ -37,4 +43,4 @@ var Filter9= (props) => {
 		</View>
 	);
 };
-export default Filter9;
+export default Filter;
--- Screens/Main.js
+++ Screens/Main.js
@@ -11,44 +11,141 @@ import {
 	ScrollView,
 } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
-import Filter12 from "../Components/Filter12";
-import Filter11 from "../Components/Filter11";
-import Filter10 from "../Components/Filter10";
-import Filter9 from "../Components/Filter9";
-import Filter8 from "../Components/Filter8";
-import Filter7 from "../Components/Filter7";
-import Filter6 from "../Components/Filter6";
-import Filter5 from "../Components/Filter5";
-import Filter4 from "../Components/Filter4";
-import Filter3 from "../Components/Filter3";
-import Filter2 from "../Components/Filter2";
-import Filter1 from "../Components/Filter1";
+import Filter from "../Components/Filter";
 
 import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
 import * as FaceDetector from "expo-face-detector";
 
-var data = [
-	{ id: "filter1", src: require("../assets/crown-pic1.png") },
-	{ id: "filter2", src: require("../assets/crown-pic2.png") },
-	{ id: "filter3", src: require("../assets/crown-pic3.png") },
-	{ id: "filter4", src: require("../assets/flower-pic1.png") },
-	{ id: "filter5", src: require("../assets/flower-pic2.png") },
-	{ id: "filter6", src: require("../assets/flower-pic3.png") },
-	{ id: "filter7", src: require("../assets/hair-pic1.png") },
-	{ id: "filter8", src: require("../assets/hat-pic1.png") },
-	{ id: "filter9", src: require("../assets/hat-pic2.png")},
-	{ id: "filter10", src: require("../assets/other-pic1.png") },
-	{ id: "filter11", src: require("../assets/other-pic2.png" )},
-	{ id: "filter12", src: require("../assets/other-pic3.png") },
-];
+const data = {
+	Crown: [
+		{ id: "crown-pic1", src: require("../assets/crown-pic1.png") },
+		{ id: "crown-pic2", src: require("../assets/crown-pic2.png") },
+		{ id: "crown-pic3", src: require("../assets/crown-pic3.png") },
+	],
+	Flowers: [
+		{ id: "flower-pic1", src: require("../assets/flower-pic1.png") },
+		{ id: "flower-pic2", src: require("../assets/flower-pic2.png") },
+		{ id: "flower-pic3", src: require("../assets/flower-pic3.png") },
+	],
+	Hairs: [{ id: "hair-pic1", src: require("../assets/hair-pic1.png") }],
+	Hats: [
+		{ id: "hat-pic1", src: require("../assets/hat-pic1.png") },
+		{ id: "hat-pic2", src: require("../assets/hat-pic2.png") },
+	],
+	Others: [
+		{ id: "other-pic1", src: require("../assets/other-pic1.png") },
+		{ id: "other-pic2", src: require("../assets/other-pic2.png") },
+		{ id: "other-pic3", src: require("../assets/other-pic3.png") },
+	],
+};
+
+const filters = {
+	"crown-pic1": {
+		src: require("../assets/crown-pic1.png"),
+		width: 3.5,
+		height: 0.7,
+		left: 0.46,
+		right: 0.15,
+		top: 1.5,
+	},
+	"crown-pic2": {
+		src: require("../assets/crown-pic2.png"),
+		width: 3.5,
+		height: 1.2,
+		left: 0.46,
+		right: 0.15,
+		top: 0.7,
+	},
+	"crown-pic3": {
+		src: require("../assets/crown-pic3.png"),
+		width: 2,
+		height: 0.6,
+		left: 0.36,
+		right: 0.15,
+		top: 1.5,
+	},
+	"flower-pic1": {
+		src: require("../assets/flower-pic1.png"),
+		width: 1.5,
+		height: 0.55,
+		left: 0.36,
+		right: 0.15,
+		top: 1.5,
+	},
+	"flower-pic2": {
+		src: require("../assets/flower-pic2.png"),
+		width: 1.2,
+		height: 0.55,
+		left: 0.36,
+		right: 0.15,
+		top: 1.3,
+	},
+	"flower-pic3": {
+		src: require("../assets/flower-pic3.png"),
+		width: 5,
+		height: 0.8,
+		left: 0.46,
+		right: 0.15,
+		top: 1.4,
+	},
+	"hair-pic1": {
+		src: require("../assets/hair-pic1.png"),
+		width: 1,
+		height: 2.5,
+		left: 0.35,
+		right: 0.5,
+		top: 0.75,
+	},
+	"hat-pic1": {
+		src: require("../assets/hat-pic1.png"),
+		width: 1.3,
+		height: 0.65,
+		left: 0.36,
+		right: 0.15,
+		top: 1.5,
+	},
+	"hat-pic2": {
+		src: require("../assets/hat-pic2.png"),
+		width: 1,
+		height: 1.5,
+		left: 0.4,
+		right: 0.1,
+		top: 0.8,
+	},
+	"other-pic1": {
+		src: require("../assets/other-pic1.png"),
+		width: 3.5,
+		height: 0.65,
+		left: 0.45,
+		right: 0.15,
+		top: 1.5,
+	},
+	"other-pic2": {
+		src: require("../assets/other-pic2.png"),
+		width: 3.5,
+		height: 0.65,
+		left: 0.45,
+		right: 0.15,
+		top: 1,
+	},
+	"other-pic3": {
+		src: require("../assets/other-pic3.png"),
+		width: 3.5,
+		height: 0.75,
+		left: 0.45,
+		right: 0.15,
+		top: 1.2,
+	},
+};
 export default class Main extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			hasCameraPermission: null,
 			faces: [],
-			current_filter: "filter1",
+			current_filter: "crown-pic1",
+			selected: "Crown",
 		};
 	}
 
@@ -101,67 +198,91 @@ export default class Main extends Component {
 						onFacesDetectionError={this.onFacesDetectionError}
 					/>
 					{this.state.faces.map((face) => {
-						if (this.state.current_filter==="filter1") {
-							return <Filter1 face={face} />;
-						} else if (this.state.current_filter==="filter2") {
-							return <Filter2 face={face} />;
-						} else if (this.state.current_filter==="filter3") {
-							return <Filter3 face={face} />;
-						} else if (this.state.current_filter==="filter4") {
-							return <Filter4 face={face} />;
-						} else if (this.state.current_filter==="filter5") {
-							return <Filter5 face={face} />;
-						} else if (this.state.current_filter==="filter6") {
-							return <Filter6 face={face} />;
-						} else if (this.state.current_filter==="filter7") {
-							return <Filter7 face={face} />
-						} else if (this.state.current_filter==="filter8") {
-							return <Filter8 face={face} />;
-						} else if (this.state.current_filter==="filter9") {
-							return <Filter9 face={face} />;
-						} else if (this.state.current_filter==="filter10") {
-							return <Filter10 face={face} />;
-						} else if (this.state.current_filter==="filter11") {
-							return <Filter11 face={face} />;
-						} else if (this.state.current_filter==="filter12") {
-							return <Filter12 face={face} />;
-						}
+						var f = filters[this.state.current_filter];
+						console.log(this.state.current_filter)
+						return (
+							<Filter
+								face={face}
+								img={f.src}
+								width={f.width}
+								height={f.height}
+								left={f.left}
+								right={f.right}
+								top={f.top}
+							/>
+						);
 					})}
 				</View>
 
 				<View style={styles.bottomContainer}>
+					<View style={styles.topBottomContainer}>
 						<ScrollView
 							contentContainerStyle={styles.scrollContainer}
 							horizontal
 							showsHorizontalScrollIndicator={false}
 						>
-						{data.map((facedata) => {
+							{Object.keys(data).map((category) => {
 								return (
 									<TouchableOpacity
-										key={facedata.id}
+										key={category.id}
 										style={[
-											styles.button,
+											styles.category,
 											{
-												borderColor:
-													this.state.current_filter === facedata.id
-														? "red"
-														: "#b",
+												backgroundColor:
+													this.state.selected === category
+														? "rgba(32, 154, 214, 0.4)"
+														: "rgba(235, 52, 180, 0.2)",
 											},
 										]}
 										onPress={() => {
 											this.setState({
-												current_filter: facedata.id,
+												selected: category,
 											});
 										}}
 									>
-										<Image
-											source={facedata.src}
-											style={styles.filter_image}
-										></Image>
+										<Text>{category}</Text>
 									</TouchableOpacity>
 								);
 							})}
 						</ScrollView>
+					</View>
+					<View
+						style={{
+							borderBottomColor: "black",
+							borderBottomWidth: StyleSheet.hairlineWidth,
+						}}
+					/>
+					<View style={styles.lowerBottomContainer}>
+						<ScrollView
+							contentContainerStyle={styles.scrollContainer}
+							horizontal
+							showsHorizontalScrollIndicator={false}
+						>
+							{data[this.state.selected].map((filter_data) => {
+								return (
+									<TouchableOpacity
+										key={`${filter_data.id}`}
+										style={[
+											styles.filterButton,
+											{
+												borderColor:
+													this.state.current_filter === filter_data.id
+														? "#FFA384"
+														: "#FFFF",
+											},
+										]}
+										onPress={() =>
+											this.setState({
+												current_filter: `${filter_data.id}`,
+											})
+										}
+									>
+										<Image source={filter_data.src} />
+									</TouchableOpacity>
+								);
+							})}
+						</ScrollView>
+					</View>
 				</View>
 			</View>
 		);
@@ -190,30 +311,42 @@ const styles = StyleSheet.create({
 	middleContainer: {
 		flexDirection: "row",
 
-		flex: 0.8,
+		flex: 0.7,
 	},
 	bottomContainer: {
-		flex: 0.1,
+		flex: 0.2,
 		backgroundColor: "#e3e186",
-
 	},
-	scrollContainer: {
-
-
+	topBottomContainer: {
+		flex: 0.3,
 	},
+	lowerBottomContainer: {
+		flex: 0.7,
+	},
+	scrollContainer: {},
 	filter_image: {
 		width: 50,
-		height: 50
+		height: 50,
 	},
-	button: {
-		height: RFValue(70),
-		width: RFValue(70),
-	justifyContent: "center",
-    alignItems: "center",
-    borderRadius: RFValue(35),
-    backgroundColor: "#E7F2F8",
-    borderWidth: 5,
-    marginRight: RFValue(20),
-    marginBottom: RFValue(10)
-	}
+	category: {
+		height: RFValue(40),
+		width: RFValue(80),
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: RFValue(35),
+		borderWidth: 3,
+		marginRight: RFValue(20),
+		marginBottom: RFValue(10),
+	},
+	filterButton: {
+		height: RFValue(90),
+		width: RFValue(90),
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgba(32, 154, 214, 0.4)",
+		borderRadius: RFValue(35),
+		borderWidth: 3,
+		marginRight: RFValue(20),
+		marginTop: RFValue(7),
+	},
 });
